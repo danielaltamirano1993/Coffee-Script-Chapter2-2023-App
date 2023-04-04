895a63a8408dacd08bc7c4c77ba49e9772d5484e
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-04-04 11:11:54 -0500

    Create extramain.js

---
+++ extrasInmainJS/extramain.js
@@ -0,0 +1,15 @@
+/*
+  # Title screen
+  c = gfx.ctx
+  
+  # Orange rectangle with a dark-gray border
+  c.fillStyle = "orange"
+  c.fillRect 10, 10, 300, 80
+  c.strokeStyle = "#3f3f3f"
+  c.strokeRect 10, 10, 300, 80
+  
+  # Professor Digman-Rünner text
+  c.fillStyle = "#202020"
+  c.font = "14pt monospace"
+  c.fillText "Professor Digman-Rünner", 30, 55
+  */
